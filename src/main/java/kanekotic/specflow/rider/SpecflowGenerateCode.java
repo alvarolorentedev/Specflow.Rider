d06b2282953fc0575fb4e0fcff35a53bb909ddc5
alvaro <alvarojosepl@gmail.com>
2017-08-23 09:50:35 +0200

    added references to use graddle to build plugin

---
+++ src/main/java/kanekotic/specflow/rider/SpecflowGenerateCode.java
@@ -0,0 +1,12 @@
+package kanekotic.specflow.rider;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class SpecflowGenerateCode extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
---
+++ src/test/java/kanekotic/specflow/rider/SpecflowGenerateCodeTest.java
@@ -0,0 +1,7 @@
+package kanekotic.specflow.rider;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpecflowGenerateCodeTest {
+
+}
