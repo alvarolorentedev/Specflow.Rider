b7ad3fb77a625ff00f00cb30142c5614f0281807
alvaro <alvarojosepl@gmail.com>
2017-08-28 09:19:58 +0200

    started reading the file from the context

--- src/main/java/kanekotic/specflow/rider/SpecflowGenerateCode.java
+++ src/main/java/kanekotic/specflow/rider/SpecflowGenerateCode.java
@@ -2,11 +2,19 @@ package kanekotic.specflow.rider;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.vfs.VirtualFile;
 
 public class SpecflowGenerateCode extends AnAction {
 
+    public SpecflowGenerateCode(){
+        super();
+    }
+
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        VirtualFile file = e.getData(PlatformDataKeys.VIRTUAL_FILE);
+
     }
 }
--- src/test/java/kanekotic/specflow/rider/SpecflowGenerateCodeTest.java
+++ src/test/java/kanekotic/specflow/rider/SpecflowGenerateCodeTest.java
@@ -1,14 +1,27 @@
 package kanekotic.specflow.rider;
 
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.junit.Test;
-import static org.mockito.Mockito.*;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class SpecflowGenerateCodeTest {
 
     @Test
-    public void actionPerformedTest(){
-        SpecflowGenerateCode action = new SpecflowGenerateCode();
-        //action.actionPerformed();
+    public void actionAnalizesFile(){
+        SpecflowGenerateCode genrator = new SpecflowGenerateCode();
+        AnActionEvent action = mock(AnActionEvent.class);
+        VirtualFile file = mock(VirtualFile.class);
+
+        when(action.getData(PlatformDataKeys.VIRTUAL_FILE)).thenReturn(file);
+
+        genrator.actionPerformed(action);
+
+        verify(action).getData(PlatformDataKeys.VIRTUAL_FILE);
     }
 
 }
