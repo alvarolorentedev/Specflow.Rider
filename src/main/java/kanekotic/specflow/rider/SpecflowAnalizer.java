0a3eb165e3eec21707364e1eb6fedf0fce2c8dbc
alvaro <alvarojosepl@gmail.com>
2017-09-08 11:41:36 +0200

    added test to obtain ast of the file content

--- src/main/java/kanekotic/specflow/rider/SpecflowAnalizer.java
+++ src/main/java/kanekotic/specflow/rider/SpecflowAnalizer.java
@@ -3,17 +3,25 @@ package kanekotic.specflow.rider;
 import gherkin.Parser;
 import gherkin.ast.GherkinDocument;
 import gherkin.pickles.Compiler;
+import gherkin.pickles.Pickle;
+
+import java.util.List;
 
 public class SpecflowAnalizer implements ISpecflowAnalizer {
 
+    private Compiler compiler;
+    private Parser<GherkinDocument> parser;
+
     public SpecflowAnalizer(Compiler compiler, Parser<GherkinDocument> parser) {
+        this.compiler = compiler;
+        this.parser = parser;
     }
 
     @Override
     public SpecflowFileContents analize(String file) {
-
+        GherkinDocument gherkinDocument = parser.parse(file);
+        List<Pickle> pickles = compiler.compile(gherkinDocument);
         return new SpecflowFileContents();
-
     }
 
 }
--- src/test/java/kanekotic/specflow/rider/SpecflowAnalizerTest.java
+++ src/test/java/kanekotic/specflow/rider/SpecflowAnalizerTest.java
@@ -3,19 +3,31 @@ package kanekotic.specflow.rider;
 import gherkin.Parser;
 import gherkin.ast.GherkinDocument;
 import gherkin.pickles.Compiler;
+import gherkin.pickles.Pickle;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class SpecflowAnalizerTest {
 
     @Test
-    public void analize() throws Exception {
+    public void analizeCallsCorrectProcess() throws Exception {
         Parser<GherkinDocument> parser = (Parser<GherkinDocument>) mock(Parser.class);
         Compiler compiler = mock(Compiler.class);
         SpecflowAnalizer analizer = new SpecflowAnalizer(compiler, parser);
-        SpecflowFileContents contents = analizer.analize("");
+        String document = "my string";
+        GherkinDocument gherkinDocument = mock(GherkinDocument.class);
+        List<Pickle> pickles = (List<Pickle>) mock(List.class);
+        when(parser.parse(document)).thenReturn(gherkinDocument);
+        when(compiler.compile(gherkinDocument)).thenReturn(pickles);
+
+        SpecflowFileContents contents = analizer.analize(document);
+
+        verify(parser).parse(document);
+        verify(compiler).compile(gherkinDocument);
         assertEquals("", contents.feature);
         assertEquals("", contents.steps);
     }
